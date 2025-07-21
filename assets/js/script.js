c3dcff3a0c205f570b3fd2016c3fb4ff3e454da6
Ese-oghene <oghenee08@gmail.com>
2025-07-21 12:43:44 +0100

    add new

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,3 +130,53 @@ const initAccordion = function (currentAccordion) {
 }
 
 for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
+
+
+
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+   const sr = ScrollReveal({
+          origin: 'top',
+          distance: '80px',
+          duration: 2000,
+          reset: true     
+   })
+
+    /* -- HOME -- */
+    // sr.reveal('.featured-text-card',{})
+    sr.reveal('.featured-name',{delay: 100})
+    sr.reveal('.hero-title',{delay: 200})
+    sr.reveal('.hero-text',{delay: 200})
+    sr.reveal('.btn-primary',{delay: 200})
+    sr.reveal('.img-holder',{delay: 300})
+    
+
+  /* -- PROJECT BOX -- */
+  sr.reveal('.project-box',{interval: 200})
+
+  /* -- HEADINGS -- */
+  sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+
+  /* -- ABOUT INFO & CONTACT INFO -- */
+  const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+  
+  srLeft.reveal('.about-info',{delay: 100})
+  srLeft.reveal('.contact-info',{delay: 100})
+
+  /* -- ABOUT SKILLS & FORM BOX -- */
+  const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  })
+  
+  srRight.reveal('.skills-box',{delay: 100})
+  srRight.reveal('.form-control',{delay: 100})
+  
